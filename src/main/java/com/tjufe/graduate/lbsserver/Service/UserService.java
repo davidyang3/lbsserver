5f0a46fd1e08cb1a2d01aa32e50816d370f6d769
David <893848044@qq.com>
2019-03-30 12:10:38 +0800

    support image

--- src/main/java/com/tjufe/graduate/lbsserver/Service/UserService.java
+++ src/main/java/com/tjufe/graduate/lbsserver/Service/UserService.java
@@ -326,36 +326,6 @@ public class UserService {
         }
     }
 
-    public static boolean generateImage(String imgStr, String imagePath)
-    {   //对字节数组字符串进行Base64解码并生成图片
-        if (imgStr == null) //图像数据为空
-            return false;
-        BASE64Decoder decoder = new BASE64Decoder();
-        try
-        {
-            //Base64解码
-            byte[] b = decoder.decodeBuffer(imgStr);
-            for(int i=0;i<b.length;++i)
-            {
-                if(b[i]<0)
-                {//调整异常数据
-                    b[i]+=256;
-                }
-            }
-            //生成jpeg图片
-            OutputStream out = new FileOutputStream(imagePath);
-            out.write(b);
-            out.flush();
-            out.close();
-            return true;
-        }
-        catch (Exception e)
-        {
-            log.error("========{}", e.getMessage());
-            return false;
-        }
-    }
-
     @Transactional
     public String updateNickName(String userId, String nickName) {
         Optional<User> userOptional = userDao.findById(userId);
